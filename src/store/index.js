24e3590b610884d89e85bc1d5060856fcbafb968
SilverFlame83 <imren_83@abv.bg>
2023-02-01 19:44:25 +0200

    index.js implemented

---
+++ src/store/index.js
@@ -0,0 +1,20 @@
+import {createStore} from 'redux';
+
+const reduxCounter = (state = { counter: 0 }, action) => {
+  if (action.type === "increment") {
+    return {
+      counter: counter + 1,
+    };
+  }
+  if(action.type === "decrement"){
+    return {
+        counter: counter -1,
+      };
+  }
+
+  return state;
+};
+
+const store = createStore(reduxCounter);
+
+export default store;
