ce224ee005748780eb22289b940b4eefbbb63230
SilverFlame83 <imren_83@abv.bg>
2023-02-03 12:02:44 +0200

    toggle implemented

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -5,12 +5,19 @@ import classes from "./Counter.module.css";
 const Counter = () => {
   const dispatch = useDispatch();
   const counter = useSelector((state) => state.counter);
+  const show = useSelector(state=> state.showCounter)
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({type: 'toggle'})
+  };
 
   const incrementHandler = () => {
     return dispatch({ type: "increment" });
   };
+
+  const increaseHandler = ()=>{
+    return dispatch({type:'increase', amount:5})
+  }
 
   const decrementHandler = () => {
     return dispatch({ type: "decrement" });
@@ -19,9 +26,10 @@ const Counter = () => {
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show &&<div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increase by 5</button>
         <button onClick={decrementHandler}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,31 @@
-import {createStore} from 'redux';
+import { createStore } from "redux";
 
-const reduxCounter = (state = { counter: 0 }, action) => {
+const initialState = { counter: 0, showCounter: true };
+
+const reduxCounter = (state = initialState, action) => {
   if (action.type === "increment") {
     return {
       counter: state.counter + 1,
+      showCounter: state.showCounter,
     };
   }
-  if(action.type === "decrement"){
+  if (action.type === "increase") {
     return {
-        counter: state.counter -1,
-      };
+      counter: state.counter + action.amount,
+      showCounter: state.showCounter,
+    };
+  }
+  if (action.type === "decrement") {
+    return {
+      counter: state.counter - 1,
+      showCounter: state.showCounter,
+    };
+  }
+  if (action.type === "toggle") {
+    return {
+      showCounter: !state.showCounter,
+      counter: state.counter,
+    };
   }
 
   return state;
