c37d70d960275d1b23cc3066ff3beaa2723fa7e6
SilverFlame83 <imren_83@abv.bg>
2023-02-04 12:37:06 +0200

    header and auth added in App component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import Counter from './components/Counter';
+import { Fragment } from "react";
 
+import Counter from "./components/Counter";
+import Auth from "./components/Auth";
+import Header from "./components/Header";
+import UserProfile from "./components/UserProfile";
 
 function App() {
   return (
-    <Counter />
+    <Fragment>
+      <Header/>
+      <Auth/>
+      <Counter />
+    </Fragment>
   );
 }
 
